9834f0a13e2b4552a3d9718c72a700f174617a77
Julianna Langston <julianna@cosaic.io>
2023-09-22 21:23:51 +0300

    Add examples using relatively positioned containers

--- samples/main.tsx
+++ samples/main.tsx
@@ -3,7 +3,8 @@ import ReactDOM from "react-dom/client";
 import Charts from "./react/index";
 import {generateCharts} from "./makeChart";
 
-generateCharts(document.querySelector<HTMLDivElement>('#app')!);
+generateCharts(document.querySelector('#app') as HTMLDivElement);
+generateCharts(document.querySelector('#position') as HTMLDivElement);
 
 const root = ReactDOM.createRoot(document.getElementById("react-root") as HTMLDivElement);
 root.render(
