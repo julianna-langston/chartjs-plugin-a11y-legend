24ae86e4239fa8504daefd203e95549a1d9f15e6
Julianna Langston <julianna@cosaic.io>
2023-09-22 20:50:20 +0300

    ts tidying

--- samples/main.tsx
+++ samples/main.tsx
@@ -5,7 +5,7 @@ import {generateCharts} from "./makeChart";
 
 generateCharts(document.querySelector<HTMLDivElement>('#app')!);
 
-const root = ReactDOM.createRoot(document.getElementById("react-root"));
+const root = ReactDOM.createRoot(document.getElementById("react-root") as HTMLDivElement);
 root.render(
     <Charts />
 );
